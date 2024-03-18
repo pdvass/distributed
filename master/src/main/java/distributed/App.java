302b63ac7a3ab9a23f099e1284df3cff9a99a18c
Pantelis Vassilopoulos <pantelisvassilopoulos@Panteliss-MBP.home>
2024-03-18 14:17:08 +0200

    Add testNewCapabilities method for self-reported reasons, as well as driver code for JSONParser

--- master/src/main/java/distributed/App.java
+++ master/src/main/java/distributed/App.java
@@ -1,5 +1,10 @@
 package distributed;
 
+import org.json.simple.parser.*;
+
+import java.io.FileReader;
+
+import org.json.simple.*;;
 /**
  * Hello world!
  *
@@ -8,9 +13,26 @@ public class App
 {
    public static void main( String[] args )
     {
+        testNewCapabilites();
+
         Terminal term = new Terminal();
         term.setup();
         term.init();
+    }
+
+    public static void testNewCapabilites(){
+        try {
+            JSONParser parser = new JSONParser();
+            JSONObject data = (JSONObject) parser.parse(new FileReader("src/main/java/distributed/data/initialData.json"));
+            System.out.println(( (JSONObject) data.get("hotels")).get("Four Seasons").toString());
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e.getMessage());
+            System.out.println("CWD: " + System.getProperty("user.dir"));
+        }
+
+        System.out.println("Hello");
+    
     }
 
 }
