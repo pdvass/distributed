63cc554a05fc49936f09805374b047a0a367d9ba
MariaPanagou <mariapanagou01@gmail.com>
2024-03-19 17:01:06 +0200

    Deleted a commented out dependency

--- master/src/test/java/distributed/Parser/JSONFileParserTest.java
+++ master/src/test/java/distributed/Parser/JSONFileParserTest.java
@@ -1,69 +1,69 @@
-package distributed.Parser;
+// package distributed.Parser;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.fail;
+// import static org.junit.Assert.assertTrue;
+// import static org.junit.Assert.assertThrows;
+// import static org.junit.Assert.fail;
 
-import java.io.FileNotFoundException;
+// import java.io.FileNotFoundException;
 
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-import org.junit.Test;
+// import org.json.simple.JSONObject;
+// import org.json.simple.parser.JSONParser;
+// import org.json.simple.parser.ParseException;
+// import org.junit.Test;
 
-public class JSONFileParserTest {
+// public class JSONFileParserTest {
 
-    @Test
-    public void testParseValidity(){
-        // Contents of dummy data file in a String.
-        String rightAnswer = """
-            {
-                "hotels": {
-                    "Pergamos": {
-                        "name": "Pergamos",
-                        "rooms": {
-                            "room1": {
-                                "id": "PergamosRoom1",
-                                "startDate": "11/04/2024",
-                                "endDate": "30/04/2024"
-                            }
-                        },
-                        "region": "Metaksourgio"
-                    }
-                }
-            }
-                """;
+//     @Test
+//     public void testParseValidity(){
+//         // Contents of dummy data file in a String.
+//         String rightAnswer = """
+//             {
+//                 "hotels": {
+//                     "Pergamos": {
+//                         "name": "Pergamos",
+//                         "rooms": {
+//                             "room1": {
+//                                 "id": "PergamosRoom1",
+//                                 "startDate": "11/04/2024",
+//                                 "endDate": "30/04/2024"
+//                             }
+//                         },
+//                         "region": "Metaksourgio"
+//                     }
+//                 }
+//             }
+//                 """;
         
-        JSONFileParser parser = new JSONFileParser("src/main/java/distributed/data/dummyData.json");
-        JSONObject data = null;
-        try{
-            data = parser.parseFile();
-        } catch (Exception e){
-            fail("Exception should not be thrown while reading the file.");
-        }
+//         JSONFileParser parser = new JSONFileParser("src/main/java/distributed/data/dummyData.json");
+//         JSONObject data = null;
+//         try{
+//             data = parser.parseFile();
+//         } catch (Exception e){
+//             fail("Exception should not be thrown while reading the file.");
+//         }
 
-        // Parsing the String into the same format as the files contents.
-        JSONParser JSONparser = new JSONParser();
-        JSONObject rightAnswerJSON = null;
+//         // Parsing the String into the same format as the files contents.
+//         JSONParser JSONparser = new JSONParser();
+//         JSONObject rightAnswerJSON = null;
 
-        try {
-            rightAnswerJSON = (JSONObject) JSONparser.parse(rightAnswer);
-        } catch (ParseException e) {
-            System.out.println(e.getMessage());
-        }
+//         try {
+//             rightAnswerJSON = (JSONObject) JSONparser.parse(rightAnswer);
+//         } catch (ParseException e) {
+//             System.out.println(e.getMessage());
+//         }
 
-        // Converting both the right answer and the data of the file
-        // into the same format Strings
-        String dataString = data.toString();
-        String rightAnswerString = rightAnswerJSON.toString();
+//         // Converting both the right answer and the data of the file
+//         // into the same format Strings
+//         String dataString = data.toString();
+//         String rightAnswerString = rightAnswerJSON.toString();
 
-        assertTrue(dataString.equals(rightAnswerString));
-    }
+//         assertTrue(dataString.equals(rightAnswerString));
+//     }
 
-    @Test
-    public void testExceptionFromParseFile(){
-        JSONFileParser parser = new JSONFileParser("dummyData.json");
-        assertThrows(FileNotFoundException.class, () -> parser.parseFile());
-    }
+//     @Test
+//     public void testExceptionFromParseFile(){
+//         JSONFileParser parser = new JSONFileParser("dummyData.json");
+//         assertThrows(FileNotFoundException.class, () -> parser.parseFile());
+//     }
     
-}
+// }
