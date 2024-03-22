1dc70de92009c94320bc97fa0d57f35cc1c0b6d9
pdvass <pantelisvassilopoulos@Panteliss-MBP.home>
2024-03-23 01:14:24 +0200

    Add javadoc to methods

--- master/src/main/java/distributed/JSONFileSystem/JSONDirManager.java
+++ master/src/main/java/distributed/JSONFileSystem/JSONDirManager.java
@@ -17,6 +17,10 @@ public class JSONDirManager {
     private final String path = "src/main/java/distributed/data/";
     private ArrayList<File> fileList = new ArrayList<>();
 
+    /**
+     * Empty constructor that reads all the JSON files on the path folder. 
+     * Can be extended with DFS to organise the folder with subfolders.
+     */
     public JSONDirManager(){
         File files = new File(this.path);
         for(File file : files.listFiles()){
@@ -32,6 +36,15 @@ public class JSONDirManager {
         return extension;
     }
 
+    /**
+     * Uses JSONFileParser as a tool to create a new, almost empty, JSON file
+     * which represents the new Hotel. Room array is initially empty.
+     * 
+     * @param name Name of the hotel
+     * @param region Region on which the hotel is located.
+     * 
+     * @see JSONFileParser#createHotelJSON(String, String)
+     */
     public void addHotel(String name, String region){
         name = name.replaceAll(" ", "");
         String fileName = this.path + name + ".json";
@@ -52,6 +65,15 @@ public class JSONDirManager {
 
     }
 
+    /**
+     * Adds room to a Room array of an already existing hotel between a date range.
+     * 
+     * @param name Name of the hotel of which the room is going to be added.
+     * @param startDate String in the format "dd/MM/yyyy" representing the first day
+     * the hotel is available
+     * @param endDate String in the format "dd/MM/yyyy" representing the last day
+     * the hotel is available
+     */
     @SuppressWarnings("unchecked")
     public void addRoom(String name, String startDate, String endDate){
         name = name.replaceAll(" ", "");
@@ -73,7 +95,7 @@ public class JSONDirManager {
         rooms.add(room);
 
         String json = data.toJSONString();
-
+        // Rewrite the whole folder with the new room added.
         try {
             FileWriter writer = new FileWriter(fileName);
             writer.write(json);
@@ -83,6 +105,10 @@ public class JSONDirManager {
         }
     }
 
+    /**
+     * Removes a hotel by deleting its corresponding JSON file.
+     * @param name The name of hotel that is going to be deleeted.
+     */
     public void removeHotel(String name){
         name = name.replaceAll(" ", "");
         String fileName = this.path + name + ".json";    
@@ -96,6 +122,12 @@ public class JSONDirManager {
         }    
     }
 
+    /**
+     * Removes a room from the room array of a hotel.
+     * 
+     * @param name The name of the hotel
+     * @param roomNumber The room number that is going to be deleted. 
+     */
     @SuppressWarnings("unchecked")
     public void removeRoom(String name, int roomNumber){
         name = name.replaceAll(" ", "");
@@ -118,6 +150,7 @@ public class JSONDirManager {
 
         String json = data.toJSONString();
 
+        // Rewrite the whole folder with the room removed.
         try {
             FileWriter writer = new FileWriter(fileName);
             writer.write(json);
