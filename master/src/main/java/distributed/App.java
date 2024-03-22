e86d65372cd9905f9611ee697479624d35f8b7a1
pdvass <pantelisvassilopoulos@Panteliss-MacBook-Pro.local>
2024-03-22 12:48:22 +0200

    Delete 'distributed' file and minor changes

--- master/src/main/java/distributed/App.java
+++ master/src/main/java/distributed/App.java
@@ -1,7 +1,8 @@
 package distributed;
-import distributed.JSONFileSystem.JSONFileParser;
 
-import org.json.simple.*;
+// import java.util.concurrent.TimeUnit;
+import distributed.JSONFileSystem.JSONDirManager;
+
 /**
  * Project's entry point.
  *
@@ -21,23 +22,18 @@ public class App
 
     public static void testNewCapabilites(){
         System.out.println("Hello");
+        JSONDirManager manager = new JSONDirManager();
+        manager.addHotel("Hotel Victory", "Viktoria");
 
-        JSONFileParser parser = new JSONFileParser("FourSeasons.json");
-        JSONObject data = null;
-        try {
-            data = parser.parseFile();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        parser.iterateJSON(data);
-        parser.updatePath("Pergamos");
-        try {
-            data = parser.parseFile();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        parser.iterateJSON(data);
+        manager.addRoom("Hotel Victory", "19/05/2024", "29/05/2024");
+
+        // try{
+        //     TimeUnit.SECONDS.sleep(5);
+        // } catch (Exception e){
+        //     System.out.println(e.getMessage());
+        // }
+
+        // manager.removeRoom("Hotel Victory", 2);
 
     }
-
 }
