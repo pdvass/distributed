6ad07f9dd4b84041e55cd2fd13d473c9078d4e5a
pdvass <pantelovas@gmail.com>
2024-04-01 11:50:04 +0300

    Add comments

--- master/src/main/java/distributed/Server/Response.java
+++ master/src/main/java/distributed/Server/Response.java
@@ -6,7 +6,13 @@ import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 /**
- * More like ClientHandler. Might rename.
+ * Request is used to abstract the connection from the Terminal. Works as
+ * a middle man between ClientHandler and Server.
+ * 
+ * @see distributed.Server.ClientHandler
+ * @see distributed.Server.Server
+ * 
+ * @author pdvass
  */
 public class Response extends Thread {
     private Socket connection = null;
@@ -14,6 +20,15 @@ public class Response extends Thread {
     private ObjectInputStream ois = null;
     private ObjectOutputStream oos = null;
 
+    /**
+     * Basic constructor that takes the Connection and the initial Contents
+     * that should be sent.
+     * 
+     * @param conn Socket connection, connected with the client.
+     * @param contents Contents of the message abstracted to an Object.
+     * 
+     * @apiNote Connection must be enstablished before initializing the Request Constructor.
+     */
     public Response(Socket conn, Object contents) throws IOException{
         this.connection = conn;
         this.contents = contents;
@@ -21,15 +36,26 @@ public class Response extends Thread {
         this.oos = new ObjectOutputStream(this.connection.getOutputStream());
     }
 
+    /**
+     * Change the contents of the Request to send.
+     * 
+     * @param newContents Contents of the message abstracted to an Object.
+     */
     protected void changeContents(Object newContents){
         this.contents = newContents;
     }
     
+    /**
+     * Sends an Object through the enstablished connection.
+     */
     protected void sendObject() throws IOException{
         this.oos.writeObject(this.contents);
         this.oos.flush();
     }
 
+    /**
+     * Receives a Object through the enstablished connection.
+     */
     protected Object readObject(){
         Object obj = null;
         try {
@@ -40,11 +66,22 @@ public class Response extends Thread {
         return obj;
     }
 
+     /**
+     * Receives a string message through the enstablished connection.
+     *
+     * @throws IOException
+     */
     protected String readMessage() throws IOException {
         String response = this.ois.readUTF();
         return response;
     }
 
+    /**
+     * Sends a string message through the enstablished connection.
+     * response class is responsible for casting it to String.
+     *
+     * @throws IOException
+     */
     protected void sendMessage() throws IOException {
         this.oos.writeUTF((String) this.contents);
         this.oos.flush();
